4bc9dda9813ce6cbee422d3d1b7e71af48a9c5f5
SYZART <assyaugi@gmail.com>
2021-07-27 22:58:05 +0700

    Update Campaign Endpoint

--- handler/campaign.go
+++ handler/campaign.go
@@ -77,3 +77,35 @@ func (h *campaignHandler) CreateCampaign(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) UpdatedCampaign(c *gin.Context) {
+	var inputID campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&inputID)
+	if err != nil {
+		response := helper.APIResponse("Failed to update campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	var inputData campaign.CreateCampaignInput
+	err = c.ShouldBindJSON(&inputData)
+	if err != nil {
+		errors := helper.FormatError(err)
+		errorMessage := gin.H{"errors": errors}
+		respponse := helper.APIResponse("Failed to  update  campaign", http.StatusUnprocessableEntity, "error", errorMessage)
+
+		c.JSON(http.StatusUnprocessableEntity, respponse)
+		return
+	}
+	currentUser := c.MustGet("currentUser").(user.User)
+	inputData.User = currentUser
+	updatedCampign, err := h.campaignService.UpdateCampaign(inputID, inputData)
+	if err != nil {
+		response := helper.APIResponse("Failed to updated of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Updated campaign succesfully", http.StatusOK, "success", campaign.FormatCampaign(updatedCampign))
+
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -40,15 +40,16 @@ func main() {
 	router := gin.Default()
 	router.Static("/images", "./images")
 	api := router.Group("/api/v1")
-	//path user
+	//routing user
 	api.POST("/user", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
-	//path campaigns
+	//routing campaigns
 	api.GET("/campaigns", campaignHandler.GetCampaigns)
 	api.GET("/campaigns/:id", campaignHandler.GetCampaign)
 	api.POST("/campaigns", authMiddleware(authService, userService), campaignHandler.CreateCampaign)
+	api.PUT("/campaigns/:id", authMiddleware(authService, userService), campaignHandler.UpdatedCampaign)
 
 	router.Run()
 }
